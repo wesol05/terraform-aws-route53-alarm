0295b46c51e40c17858e005325447936807f4299
Tomasz Wesołowski <wesol05@gmail.com>
2019-07-31 14:44:13 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_sns_topic" "topic" {
+  name = var.sns-topic
+}
+
+resource "aws_route53_health_check" "health_check" {
+  count = var.enabled ? 1 : 0
+
+  type          = "HTTPS"
+  fqdn          = var.domain
+  resource_path = var.path
+  port          = 443
+
+  failure_threshold = var.failure_threshold
+  request_interval = var.request_interval
+
+  tags = merge(var.tags, {Name: var.domain})
+}
+
+resource "aws_cloudwatch_metric_alarm" "alarm" {
+  count = var.enabled ? 1 : 0
+  alarm_name = var.domain
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods = var.evaluation_periods
+  statistic = "Minimum"
+  threshold = 1
+  period = var.period
+  treat_missing_data = "missing"
+
+  dimensions = {
+    HealthCheckId = aws_route53_health_check.health_check[count.index].id
+  }
+  metric_name = "HealthCheckStatus"
+  namespace = "AWS/Route53"
+
+
+  alarm_actions = [ data.aws_sns_topic.topic.arn ]
+
+  tags = var.tags
+}
+
---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "domain" {
+  description = "The fully qualified domain name of the endpoint to be checked."
+}
+variable "path" {
+  default = "/"
+  description = "The path that you want Amazon Route 53 to request when performing health checks."
+}
+variable "sns-topic" {
+  description = "Name of the topic where to send a notifications."
+}
+variable "failure_threshold" {
+  default = 3
+  description = "The number of consecutive health checks that an endpoint must pass or fail."
+}
+variable "request_interval" {
+  default = 30
+  description = "The number of seconds between the time that Amazon Route 53 gets a response from your endpoint and the time that it sends the next health-check request."
+}
+variable "enabled" {
+  type = bool
+  default = true
+}
+variable "tags" {
+  type = "map"
+  default = {}
+  description = "Tags attached to created resources."
+}
+variable "period" {
+  default = 60
+  description = "The period in seconds over which the specified statistic is applied."
+}
+variable "evaluation_period" {
+  default = 1
+  description = "The number of periods over which data is compared to the specified threshold."
+}
