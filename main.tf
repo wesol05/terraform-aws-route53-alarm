e44236a7cb0103fcaf01d5e5898b38c0619759bf
Tomasz Wesolowski <wesol05@gmail.com>
2021-10-28 15:28:20 +0200

    restore provider

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_sns_topic" "topic" {
+  name = var.sns-topic
+}
+
 resource "aws_route53_health_check" "health_check" {
+  count = var.enabled ? 1 : 0
+
   type          = var.https ? "HTTPS" : "HTTP"
   fqdn          = var.domain
   resource_path = var.path
@@ -12,6 +22,7 @@ resource "aws_route53_health_check" "health_check" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "alarm" {
+  count = var.enabled ? 1 : 0
   alarm_name = var.domain
   comparison_operator = "LessThanThreshold"
   evaluation_periods = var.evaluation_periods
@@ -25,6 +36,9 @@ resource "aws_cloudwatch_metric_alarm" "alarm" {
   }
   metric_name = "HealthCheckStatus"
   namespace = "AWS/Route53"
+
+
+  alarm_actions = [ data.aws_sns_topic.topic.arn ]
 
   tags = var.tags
 }
